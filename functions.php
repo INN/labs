03c20b22b1beebc085bce0a53f19a836ab5a3f00
rclations <contact@rclations.com>
2017-04-18 15:30:36 -0400

    making labs a child theme of largo 1.0

--- functions.php
+++ functions.php
@@ -15,160 +15,31 @@ if ( ! function_exists( 'largo_setup' ) ) :
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function largo_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Largo, use a find and replace
-	 * to change 'largo' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'largo', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
+function innlabs_setup() {
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'menu-1' => esc_html__( 'Primary', 'largo' ),
 		'global' => esc_html__( 'Global', 'largo' ),
 	) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'largo_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
 }
 endif;
-add_action( 'after_setup_theme', 'largo_setup' );
-
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function largo_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'largo_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'largo_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function largo_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'largo' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'largo' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-
-	$homepage_sections = get_theme_mod( 'largo_homepage_layout_settings', 5 );
-	if ( $homepage_sections || is_customize_preview() ) {
-		$count = 1;
-		while ( $count <= $homepage_sections ) {
-			$columns = get_theme_mod( "largo_homepage_layout_settings_$count", 4 );
-			$column_count = 1;
-			while ( $column_count <= $columns ) {
-				register_sidebar( array(
-					'name'          => esc_html__( "Homepage Section $count Column $column_count", 'largo' ),
-					'id'            => "section-$count-column-$column_count",
-					'description'   => esc_html__( 'Add widgets here.', 'largo' ),
-					'before_widget' => '<section id="%1$s" class="widget %2$s">',
-					'after_widget'  => '</section>',
-					'before_title'  => '<h2 class="widget-title">',
-					'after_title'   => '</h2>',
-				) );
-				$column_count++;
-			}
-			$count++;
-		}
-	}
-}
-add_action( 'widgets_init', 'largo_widgets_init' );
+add_action( 'after_setup_theme', 'innlabs_setup' );
 
 /**
  * Enqueue scripts and styles.
  */
 function largo_scripts() {
-	wp_enqueue_style( 'largo-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'innlabs-style', get_stylesheet_uri() );
 
 	wp_deregister_script('jquery');
 	wp_register_script('jquery', "https" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://code.jquery.com/jquery-3.2.1.min.js", false, null);
 	wp_enqueue_script('jquery');
-	
+
 	if ( is_front_page() && is_home() ) {
 		wp_enqueue_script( 'labs-home', get_template_directory_uri() . '/js/labs-home.js', array(), '20151215', true );
 	}
 
 	wp_enqueue_script( 'largo-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20151215', true );
-
-	wp_enqueue_script( 'largo-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'largo_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';
