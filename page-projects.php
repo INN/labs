4cf202181cbbcb5ac45ac0242bb1924e710734ef
Julia Smith <julia@inn.org>
2017-05-10 12:37:12 -0500

    projects page in progress

--- page-projects.php
+++ page-projects.php
@@ -42,47 +42,47 @@ get_header(); ?>
 					</div>
 				</div>
 
-				<h4>WordPress Plugins</h4>
-				<div id="plugins" class="flex-grid">
+				<h4>Largo+ Plugins</h4>
+				<div class="flex-grid grid-wrap">
 					<div>
-						<h3><a href="https://wordpress.org/plugins/client-hosting-manager/" target="_blank">Client Hosting Manager</a></h3>
-						<p>Active Installs: 50+</p>
+						<h3><a href="https://wordpress.org/plugins/doubleclick-for-wp/" target="_blank">DoubleClick for WordPress</a></h3>
+						<p>DoubleClick for WordPress gives site administrators an easy way to serve DFP inventory on their WordPress site.</p>
 					</div>
 					<div>
-						<h3><a href="https://wordpress.org/plugins/doubleclick-for-wp/" target="_blank">DoubleClick for WordPress</h3></h3>
-						<p>Active Installs: 100+</p>
+						<h3><a href="https://wordpress.org/plugins/ga-popular-posts/" target="_blank">Google Analytics Popular Posts</a></h3>
+						<p>This plugin queries Google Analytics for pageview data and determines a post's weighted popularity score based on publish date and total number of pageviews.</p>
 					</div>
 					<div>
-						<h3><a href="https://wordpress.org/plugins/ga-popular-posts/" target="_blank">Google Analytics Popular Posts</h3></h3>
-						<p>Active Installs: 100+</p>
+						<h3><a href="https://wordpress.org/plugins/link-roundups/" target="_blank">Link Roundups</a></h3>
+						<p>This plugin creates curated lists of links that you can display in a widget or use to generate new MailChimp campaigns.</p>
+					</div>
+				</div>
+				<div class="flex-grid grid-wrap">
+					<div>
+						<h3><a href="https://wordpress.org/plugins/news-quiz-shortcode/" target="_blank">News Quiz Shortcode</a></h3>
+						<p>This shortcode allows you to embed a news quiz created with Mother Jones’ news quiz library.</p>
 					</div>
 					<div>
-						<h3><a href="https://wordpress.org/plugins/link-roundups/" target="_blank">Link Roundups</h3></h3>
-						<p>Active Installs: 100+</p>
+						<h3><a href="https://wordpress.org/plugins/no-nonsense-google-analytics/" target="_blank">No-Nonsense Google Analytics</a></h3>
+						<p>No-Nonsense Google Analytics provides a simple way to embed multiple Universal Analytics codes on your site. No dashboard, no reports.</p>
 					</div>
 					<div>
-						<h3><a href="https://wordpress.org/plugins/news-quiz-shortcode/" target="_blank">News Quiz Shortcode</h3></h3>
-						<p>Active Installs: 40+</p>
+						<h3><a href="https://wordpress.org/plugins/npr-story-api/" target="_blank">NPR Story API</a></h3>
+						<p>Add syndicated content to your site via the NPR Story API. The available content includes audio, text, images and other content from most NPR programs dating back to 1995.</p>
+					</div>
+				</div>
+				<div class="flex-grid grid-wrap">
+					<div>
+						<h3><a href="https://wordpress.org/plugins/public-media-platform/" target="_blank">Public Media Platform</a></h3>
+						<p>Expand your reach by distributing your posts on the Public Media Platform.</p>
 					</div>
 					<div>
-						<h3><a href="https://wordpress.org/plugins/no-nonsense-google-analytics/" target="_blank">No-Nonsense Google Analytics</h3></h3>
-						<p>Active Installs: 30+</p>
+						<h3><a href="https://wordpress.org/plugins/pym-shortcode/" target="_blank">Pym Shortcode</a></h3>
+						<p>Pym Shortcode will resize an iframe responsively based on the height of its content and the width of its container – perfect for embedding interactive graphics within a post.</p>
 					</div>
 					<div>
-						<h3><a href="https://wordpress.org/plugins/npr-story-api/" target="_blank">NPR Story API</h3></h3>
-						<p>Active Installs: 50+</p>
-					</div>
-					<div>
-						<h3><a href="https://wordpress.org/plugins/public-media-platform/" target="_blank">Public Media Platform</h3></h3>
-						<p>Active Installs: 50+</p>
-					</div>
-					<div>
-						<h3><a href="https://wordpress.org/plugins/pym-shortcode/" target="_blank">Pym Shortcode</h3></h3>
-						<p>Active Installs: 60+</p>
-					</div>
-					<div>
-						<h3><a href="https://wordpress.org/plugins/super-cool-ad-inserter/" target="_blank">Super Cool Ad Inserter Plugin</h3></h3>
-						<p>Active Installs: 60+</p>
+						<h3><a href="https://wordpress.org/plugins/super-cool-ad-inserter/" target="_blank">Super Cool Ad Inserter Plugin</a></h3>
+						<p>This plugin allows you to insert ads, newsletter signups, and other calls to action into post content at set intervals.</p>
 					</div>
 				</div>
 			</div>
