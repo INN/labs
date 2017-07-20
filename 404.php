0855f581bdcc4d24a0a6fdda6292cdb784a8ea9d
Julia Smith <julia@inn.org>
2017-07-20 00:59:39 -0500

    404 template

---
+++ 404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Largo
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	<section class="section">
+		<div class="inner content" style="margin-top:100px;font-weight:bold;font-size:3em;line-height:.5;color:black;text-transfrom:uppercase;text-align:center;">
+
+			Nothing's here!      
+
+		</div>
+	</section>
+	</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
